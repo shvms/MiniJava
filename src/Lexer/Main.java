327ea76dbacc9e4cb78f7c4c0963d84949c32a7d
Shuvam Shah <shvmks@outlook.com>
2021-02-06 02:34:44 +0530

    Lexer module added

---
+++ src/Lexer/Main.java
@@ -0,0 +1,15 @@
+package Lexer;
+
+public class Main {
+
+    public static void main(String [] args) {
+        try {
+            new MiniJavaParser(System.in).Goal();
+            System.out.println("Lexical analysis successful");
+        }
+        catch (ParseException e) {
+            System.out.println("Lexer Error : \n"+ e.toString());
+        }
+    }
+}
+
