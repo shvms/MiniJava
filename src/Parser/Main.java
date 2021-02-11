9fef2347c6bd38781943e3da41997c7ee4676aaa
Shuvam Shah <shvmks@outlook.com>
2021-02-12 00:41:13 +0530

    Parser: Production rules added

--- src/Parser/Main.java
+++ src/Parser/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String [] args) {
         try {
-            new MiniJavaParser(System.in).Goal();
-            System.out.println("Lexical analysis successfull");
+            new MiniJavaParser(System.in).Program();
+            System.out.println("Parsing successful");
         }
         catch (ParseException e) {
-            System.out.println("Lexer Error : \n"+ e.toString());
+            System.out.println(e.toString());
         }
     }
 }
