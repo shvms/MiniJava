4bda518aae3bdc533dfd1af3d6cd6c00a117e3cc
Shuvam Shah <shvmks@outlook.com>
2021-02-11 18:19:48 +0530

    Parser: Lexer expanded

---
+++ src/Parser/Main.java
@@ -0,0 +1,14 @@
+package Parser;
+
+public class Main {
+
+    public static void main(String [] args) {
+        try {
+            new MiniJavaParser(System.in).Goal();
+            System.out.println("Lexical analysis successfull");
+        }
+        catch (ParseException e) {
+            System.out.println("Lexer Error : \n"+ e.toString());
+        }
+    }
+}
---
+++ src/Parser/README.md
@@ -0,0 +1,5 @@
+# Parser
+Uses *JavaCC* to generate **LL(1)** parser for the MiniJava language.
+Grammar can be found [here](http://www.cambridge.org/resources/052182060X/MCIIJ2e/grammar.htm).
+
+Note: This module supersedes module *Lexer* as it contains both Lexer & Parser.
